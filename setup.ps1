58eaf374b7b6b651a66a87ef792fe5eb84391f74
Robert Schaedler <robertschaedler3@gmail.com>
2025-01-16 21:16:11 +0000

    update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -16,13 +16,27 @@ winget install -e --id Microsoft.PowerShell
 winget install -e --id Microsoft.VisualStudioCode
 winget install -e --id JanDeDobbeleer.OhMyPosh
 
+# GitHub
 # gh auth login
 # gh repo clone 'https://github.com/robertschaedler3/.config' "$Workspace/.config"
 
-# TODO: git username and email
+# Git
+# git config --global user.name "Robert Schaedler"
+# git config --global user.email ""
 
-# TODO: windows terminal config
+# PowerShell
+New-Item $PROFILE -ItemType File -Force | Out-Null
+"oh-my-posh init pwsh --config $env:LOCALAPPDATA\Programs\oh-my-posh\themes\bubbles.omp.json" | Out-File $PROFILE -Append
 
-# TODO: PowerShell config (ohmyposh)
+# Windows Terminal
+# ...
 
+# WSL
 wsl --update
+# wsl --install -d Ubuntu
+
+# User Settings
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "AppsUseLightTheme" /t REG_DWORD /d 0 /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "AppsUseLightTheme" /t REG_DWORD /d 0 /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "EnableTransparency" /t REG_DWORD /d 1 /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "SystemUsesLightTheme" /t REG_DWORD /d 0 /f
